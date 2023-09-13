60d4ddb61d4bc2f103e2648474cff7122736e757
Amós Rodrigues <amos.adm.rh@gmail.com>
2023-09-13 20:32:13 -0300

    feat: implement route upload videos.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 import { fastify } from 'fastify'
-import { prisma } from './lib/prisma'
+import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-video'
 
 const app = fastify()
 
-app.get('/prompts', async () => {
-  const prompts = await prisma.prompt.findMany()
-
-  return prompts
-})
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
 
 app
   .listen({
