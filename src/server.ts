1f53abc898534472217070e6b7b18f7b53f931ba
Amós Rodrigues <amos.adm.rh@gmail.com>
2023-09-13 08:52:31 -0300

    feat: setup prisma and route get prompts.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import { fastify } from 'fastify'
+import { prisma } from './lib/prisma'
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello World'
+app.get('/prompts', async () => {
+  const prompts = await prisma.prompt.findMany()
+
+  return prompts
 })
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP Server Running!');
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
